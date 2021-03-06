33a95545d233d39fd18f3587f064c854c0467f82
Avanish Gupta <ui19co15@iiitsurat.ac.in>
2021-03-06 10:13:12 +0530

    Written Dart code. Used hot reload for a faster development cycle. Implemented a stateful widget, adding interactivity to your app. Created a route and added logic for moving between the home route and the new route. Learned about changing the look of your app's UI using themes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart'; // Importing a new package
 
+// Entry point of the application
 void main() => runApp(MyApp());
 
+// overriding the application default, if exists
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final WordPair wordPair = WordPair.random(); // Delete this line.
     return MaterialApp(
       title: 'Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.green,
+      ),
       home: RandomWords(),
     );
   } // build
 }
 
 class RandomWordsState extends State<RandomWords> {
-  // final WordPair wordPair = WordPair.random();
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   @override // Add from this line ...
   Widget build(BuildContext context) {
-    // return Text(wordPair.asPascalCase);
     return Scaffold(
       appBar: AppBar(
         title: Text("Name Gernator"),
+        // Routing from here
+        actions: <Widget>[
+          // Add 3 lines from here...
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
-  } // ... to this line.
+  } // End of Widget body
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
 
   // Widget of suggestions
   Widget _buildSuggestions() {
@@ -58,11 +97,27 @@ class RandomWordsState extends State<RandomWords> {
 
   // Build one row in a page
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      // Add the favorite button
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      // Add logic for making it favorite
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
@@ -71,26 +126,3 @@ class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => RandomWordsState();
 }
-
-/*
-Naive code for static widget
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Avanish\'s world'),
-        ),
-        body: Center(
-          // child: const Text('Hello World'),
-          child: Text(wordPair.asPascalCase),
-        ),
-      ),
-    );
-  }
-}
-*/
